ed0fdada2e91c809c6af30776e62e088115259c4
“Shashwat <shashwatgoyal2004@gmail.com>
2023-09-24 18:50:07 +0530

    add , delete method

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
-
 import './App.css';
-
 import React from 'react'
+import ListItems from './ListItems';
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { faTrash } from '@fortawesome/free-solid-svg-icons';
+
+
+library.add( faTrash );
 
 export class App extends React.Component {
   constructor(props) {
@@ -14,17 +18,61 @@ export class App extends React.Component {
         key:''
       }
     }
+    this.handleinput = this.handleinput.bind(this);
+    this.addItem= this.addItem.bind(this);
+    this.deleteItem = this.deleteItem.bind(this);
+    
+  }
+  handleinput(e) {
+    this.setState({
+      currentItem: {
+        text:e.target.value,
+        key: Date.now()
+      }
+    })
+  }
+  
+  addItem(e) {
+    e.preventDefault();
+    const newItem = this.state.currentItem;
+    // console.log(newItem);
+    if (newItem !== "") {
+      const newItems = [...this.state.items, newItem]; 
+      this.setState({
+        items: newItems,
+      currentItem: {
+        text: '',
+        key: ''
+        }
+      });
+    }
+  }
+  
+  deleteItem(key) {
+    const filterItems = this.state.items.filter(item => item.key !== key);
+    this.setState({
+      items: filterItems,
+    })
   }
   render() {
     return (
       <div>
         <header className='App'>
-        <form id="to-do-form">
-          <input type="text" placeholder='Enter Text'/>
+        <form id="to-do-form" onSubmit={this.addItem}>
+            <input type="text" placeholder='Enter Text' value={this.state.currentItem.text}
+            onChange={this.handleinput}
+            />
             <button type="submit">Add</button>
         
         </form>
-     </header></div>
+        </header>
+        <ListItems items={this.state.items}
+          deleteitem={this.deleteItem}>
+          
+
+
+        </ListItems>
+      </div>
 
     )
   }
